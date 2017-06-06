da97e8f44009a70a1a63b45b2de83b8ff857bf73
Lukasz211 <lukasz.grabarczykk@gmail.com>
2017-06-06 10:03:19 +0200

    Readme

---
+++ Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Odliczanie ...");
+		for(int i=5;i>=0;i--){
+			System.out.println( i );
+		}
+	}
+
+}
